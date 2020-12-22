3e066b2dad9f9ff65361c909f4ae9e4d850e7e3a
Zoey Fahner <zoeyfahner@gmail.com>
2020-12-22 13:30:37 -0500

    2.1.3 and 2.1.4

--- lib/chapter_2.ex
+++ lib/chapter_2.ex
@@ -3,7 +3,7 @@ defmodule Chapter_2 do
   Chapter 2 - Induction and Recursion
   """
 
-  @spec are_equal?(list(any), list(any)) :: boolean
+  @spec are_equal?([any], [any]) :: boolean
   @doc """
   Def. 2.12 - Two sequences S and T are equal iff
   1. they are both empty or
@@ -15,4 +15,28 @@ defmodule Chapter_2 do
 
   def are_equal?([head_s | tail_s], [head_t | tail_t]),
     do: head_s == head_t and are_equal?(tail_s, tail_t)
+
+  @spec is_member_of?(any, [any]) :: false
+  @doc """
+  Def 2.1.3 - ElementOf(x, S) is
+  1. FALSE if S is empty
+  2. TRUE if S is nonempty and x = head(S)
+  2'. ElementOf(x, tail(S)) otherwise.
+  """
+  def is_member_of?(_x, []), do: false
+  def is_member_of?(x, [head_s | _tail_s]) when x == head_s, do: true
+
+  def is_member_of?(x, [_head_s | tail_s]),
+    do: is_member_of?(x, tail_s)
+
+  @spec get_length([any]) :: non_neg_integer
+  @doc """
+  Def 2.1.4 - If S is a sequence, then Length(S) is
+  1. 0, if S is empty
+  2. 1 + Length(tail(s)), if not.
+  """
+  def get_length([]), do: 0
+
+  def get_length([_s_head | s_tail]),
+    do: 1 + length(s_tail)
 end
--- test/chapter_2_test.exs
+++ test/chapter_2_test.exs
@@ -12,4 +12,23 @@ defmodule Chapter_2Test do
     refute Chapter_2.are_equal?([1], [])
     refute Chapter_2.are_equal?([], [3, 4, 5])
   end
+
+  test "2.13 - x is a member of S" do
+    # Valids
+    assert Chapter_2.is_member_of?(1, [1, 3, 5])
+    assert Chapter_2.is_member_of?(3, [1, 3, 5])
+    assert Chapter_2.is_member_of?(5, [1, 3, 5])
+    assert Chapter_2.is_member_of?(1, [1])
+    # Invalids
+    refute Chapter_2.is_member_of?(1, [])
+    refute Chapter_2.is_member_of?(1, [2])
+    refute Chapter_2.is_member_of?(1, [2, 4, 6])
+  end
+
+  test "2.14 - Length can be calcualted properly for S" do
+    assert Chapter_2.get_length([]) == 0
+    assert Chapter_2.get_length([1]) == 1
+    assert Chapter_2.get_length([1, 2]) == 2
+    assert Chapter_2.get_length([1, 2, 3, 4, 5]) == 5
+  end
 end
